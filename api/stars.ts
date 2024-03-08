9f23032130fa9c93d48612afeecfaa45f3106e54
Waritthon2004 <Waritthonzaza@gmail.com>
2024-03-09 00:11:25 +0700

    final

--- api/stars.ts
+++ api/stars.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { conn } from "../dbconnent";
 import mysql from "mysql";
-
+import { Actor, resMovie } from "../model/modelMovie";
 export const router = express.Router();
 
 // /movie
@@ -16,48 +16,53 @@ router.get("/", (req, res) => {
     }
   });
 });
-
-router.get("/:name", (req, res) => {
-  let responseData = {
-    persons: [],
-    movies: []
-  };
-
-  let sql = `SELECT Person_R.* FROM Person_R WHERE PID IN (
-    SELECT Stars_R.SPID FROM Stars_R
-    INNER JOIN Movie_R ON Stars_R.SMID = Movie_R.MID
-    WHERE Movie_R.Name LIKE ?
-
-    UNION
-
-    SELECT creators_R.CPID FROM creators_R
-    INNER JOIN Movie_R ON creators_R.CMID = Movie_R.MID
-    WHERE Movie_R.Name LIKE ?
-)`;
-  sql = mysql.format(sql, [`%${req.params.name}%`, `%${req.params.name}%`]); // Use the same parameter twice
-  conn.query(sql, (err, result) => {
-    if (err) {
-      res.status(400).json(err);
-    } else {
-      responseData.persons = result;
-      checkAndSendResponse();
-    }
-  });
-
+router.get("/:name", async (req, res) => {
   let sql1 = `SELECT distinct * FROM Movie_R WHERE Name LIKE ?`;
   sql1 = mysql.format(sql1, [`%${req.params.name}%`]);
-  conn.query(sql1, (err, result) => {
+  conn.query(sql1, async (err, result) => {
     if (err) {
       res.status(400).json(err);
     } else {
-      responseData.movies = result;
-      checkAndSendResponse();
+      let Movies : resMovie[] = result.map((movie : any)=>{
+        return{
+          MID:    movie.MID,
+          Name:   movie.Name,
+          Image:  movie.Image,
+          Type:   movie.Type,
+          detail: movie.detail,
+          rating: movie.rating,
+          day:    movie.day,
+          head:   movie.head,
+          Actor:  []
+        }
+      });
+      for(let index = 0; index < Movies.length; index++) {
+        let result1 : any= await new Promise((resolve, reject) => {
+          conn.query("SELECT distinct Person_R.* FROM Movie_R,Stars_R,Person_R WHERE Movie_R.MID=Stars_R.SMID and Stars_R.SPID = Person_R.PID and  Movie_R.Name LIKE ?",['%' + Movies[index].Name + '%'],(err,result)=>{
+            if(err) reject(err);
+            resolve(result);
+          });
+        });
+        let Actor : Actor[] = result1.map((actor : any)=>{
+          return{
+            PID:    actor.PID,
+            Name:   actor.Name,
+            Image:  actor.Image,
+            detail: actor.detail,
+          }
+        });
+        Movies[index].Actor = Actor;
+  
+        let result2 :any = await new Promise((resolve, reject) => {
+          conn.query("SELECT distinct Person_R.* FROM Movie_R,creators_R,Person_R WHERE Movie_R.MID=creators_R.CMID and creators_R.CPID = Person_R.PID and  Movie_R.Name LIKE ?",['%' + Movies[index].Name + '%'],(err,result)=>{
+            if(err) reject(err);
+            resolve(result);
+          });
+        });
+        Movies[index].Actor[Actor.length-1].Creaters = result2;
+      }
+      
+      res.status(200).json(Movies);
     }
   });
-
-  function checkAndSendResponse() {
-    if (responseData.persons.length > 0 && responseData.movies.length > 0) {
-      res.json(responseData);
-    }
-  }
 });
--- model/modelMovie.ts
+++ model/modelMovie.ts
@@ -8,3 +8,31 @@ export interface Movie {
     type:   string[];
     img:    string;
 }
+export interface resMovie {
+    MID:    number;
+    Name:   string;
+    Image:  string;
+    Type:   string;
+    detail: string;
+    rating: string;
+    day:    string;
+    head:   string;
+    Actor:  Actor[];
+}
+
+
+export interface Actor {
+    PID:    number;
+    Name:   string;
+    Image:  string;
+    detail: string;
+    Creaters : Creaters[];
+}
+
+
+export interface Creaters {
+    PID:    number;
+    Name:   string;
+    Image:  string;
+    detail: string;
+}
